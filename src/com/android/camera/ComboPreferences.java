CyanogenMod/android_packages_apps_Camera2

Camera2: Add option to set max screen brightness
* Set maximum screen brightness while the user is inside the camera app

Change-Id: I8b16ba47a933bc7d6b0c1cd62bfd6ca54875ce1e

src/com/android/camera/ComboPreferences.java
@@ -154,6 +154,7 @@ public class ComboPreferences implements
                 || key.equals(CameraSettings.KEY_TIMER_SOUND_EFFECTS)
                 || key.equals(CameraSettings.KEY_PHOTOSPHERE_PICTURESIZE)
                 || key.equals(CameraSettings.KEY_POWER_SHUTTER)
+                || key.equals(CameraSettings.KEY_MAX_BRIGHTNESS)
                 || key.equals(CameraSettings.KEY_STORAGE);
     }
 

TheXPerienceProject/android_packages_apps_Snappy

src/com/android/camera/ComboPreferences.java
@@ -159,7 +159,8 @@ public class ComboPreferences implements
                 || key.equals(SettingsManager.KEY_MONO_PREVIEW)
                 || key.equals(SettingsManager.KEY_SWITCH_CAMERA)
                 || key.equals(SettingsManager.KEY_CLEARSIGHT)
-                || key.equals(CameraSettings.KEY_POWER_SHUTTER);
+                || key.equals(CameraSettings.KEY_POWER_SHUTTER)
+                || key.equals(CameraSettings.KEY_MAX_BRIGHTNESS);
     }
 
     @Override